472e0832ddd09433be4633213a51c91229b50c3c
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2022-09-07 15:11:36 +0100

    commit -a

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i<numberOfButtons; i++) {
  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-  alert("i got clicked!");
+
+
+  this.style.color = "white";
+
  });
+
 }
 
 
 
 
+
+  // var audio = new Audio('sounds/tom-1.mp3');
+  // audio.play();
